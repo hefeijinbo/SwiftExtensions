4822db836fde3d1adf8a6219cfd0e6fae1bde667
jinbo <jinbo@huami.com>
2020-08-15 14:40:29 +0800

    CALayer UIFont 扩展

---
+++ Sources/CoreAnimation/CALayerExtensions.swift
@@ -0,0 +1,22 @@
+//
+//  CALayerExtensions.swift
+//  SwiftExtensions
+//
+//  Created by jinbo on 2020/8/15.
+//  Copyright © 2020 SwiftExtensions. All rights reserved.
+//
+
+import UIKit
+
+public extension CALayer {
+    var snapshotImage: UIImage? {
+        UIGraphicsBeginImageContextWithOptions(bounds.size, isOpaque, 0)
+        defer {
+            UIGraphicsEndImageContext()
+        }
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        render(in: context)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        return image
+    }
+}
--- Sources/UIKit/Display/UIFontExtensions.swift
+++ Sources/UIKit/Display/UIFontExtensions.swift
@@ -8,6 +8,43 @@
 
 import UIKit
 
+public enum FontType: String {
+    case None = ""
+    case Regular = "Regular"
+    case Bold = "Bold"
+    case DemiBold = "DemiBold"
+    case Light = "Light"
+    case UltraLight = "UltraLight"
+    case Italic = "Italic"
+    case Thin = "Thin"
+    case Book = "Book"
+    case Roman = "Roman"
+    case Medium = "Medium"
+    case MediumItalic = "MediumItalic"
+    case CondensedMedium = "CondensedMedium"
+    case CondensedExtraBold = "CondensedExtraBold"
+    case SemiBold = "SemiBold"
+    case BoldItalic = "BoldItalic"
+    case Heavy = "Heavy"
+}
+
+/// EZSwiftExtensions
+public enum FontName: String {
+    case HelveticaNeue
+    case Helvetica
+    case Futura
+    case Menlo
+    case Avenir
+    case AvenirNext
+    case Didot
+    case AmericanTypewriter
+    case Baskerville
+    case Geneva
+    case GillSans
+    case SanFranciscoDisplay
+    case Seravek
+}
+
 public extension UIFont {
     /// 系统字体加粗
     @objc var systemBold: UIFont {
@@ -17,5 +54,24 @@ public extension UIFont {
     /// 系统字体斜体
     @objc var systemItalic: UIFont {
         return UIFont(descriptor: fontDescriptor.withSymbolicTraits(.traitItalic)!, size: 0)
+    }
+    
+    static func getFont(name: FontName, type: FontType, size: CGFloat) -> UIFont {
+        let fontName = name.rawValue + "-" + type.rawValue
+        if let font = UIFont(name: fontName, size: size) {
+            return font
+        }
+        
+        let fontNameNone = name.rawValue
+        if let font = UIFont(name: fontNameNone, size: size) {
+            return font
+        }
+        
+        let fontNameRegular = name.rawValue + "-" + "Regular"
+        if let font = UIFont(name: fontNameRegular, size: size) {
+            return font
+        }
+        
+        return UIFont.systemFont(ofSize: size)
     }
 }
